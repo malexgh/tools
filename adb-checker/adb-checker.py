36f6fa675009b799c3050ecbdbd43a957294a378
Marcio <alexandrino.marcio@gmail.com>
2019-10-10 20:02:25 -0300

    adding some file checking

--- adb-checker/adb-checker.py
+++ adb-checker/adb-checker.py
@@ -11,36 +11,51 @@ def search_adbs(root_search):
                 found_files.append(os.path.join(root, name)+'\n')
     return found_files
 
-def write_found_files(found_files):
-    f= open("found_adbs.txt","w+")
-    f.writelines(found_files)
-    f.close()
+def write_found_adbs(found_files):
+    try:
+        #writer= open("found_adbs.txt","w+")
+        with open('found_adbs.txt', 'w') as writer:
+            writer.writelines(found_files)
+            #f.close()
+    except:
+        print("Something went wrong writing the file")
 
-def read_found_files():
-    f= open("found_adbs.txt","r")
-    lines=f.readlines()
-    f.close()
+def read_found_adbs():
+    lines=[]
+    try:
+        #reader= open("found_adbs.txt","r")
+        with open('found_adbs.txt', 'r') as reader:
+            lines=reader.readlines()
+            #reader.close() #needed?
+    except:
+        print("Something went wrong reading the file")
     return lines
 
 def check_adbs(found_files):
     for fn in found_files:
         name=fn[0:-1] #remove \n
         #print(fn,name)
-        if os.path.exists(name):
+        try:
             p=subprocess.run([name,'version'], universal_newlines=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             print(p.stdout)
             #print(p.stderr)
             #print(p.returncode)
+        except:
+            print("Something went wrong executing the file")
 
 def main():
+    print('Reading file...')
+    adbs=read_found_adbs()
     if len(sys.argv)==2:
         root_search=sys.argv[1]
         print('Searching...')
         found_files=search_adbs(root_search)
-        write_found_files(found_files)
-    else:
-        print('Reading file...')
-        found_files=read_found_files()
-    check_adbs(found_files)
+        for name in found_files:
+            if name in adbs:
+                print('Already There:', name)
+            else:
+                adbs.append(name)
+        write_found_adbs(adbs)
+    check_adbs(adbs)
 
 main()
