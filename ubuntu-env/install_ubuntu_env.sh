d8e1d17c949415bd177d342177dc5f19e1635761
Marcio <alexandrino.marcio@gmail.com>
2019-10-24 01:03:57 -0300

    check version

--- ubuntu-env/install_ubuntu_env.sh
+++ ubuntu-env/install_ubuntu_env.sh
@@ -83,24 +83,30 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get update
 sudo apt-get -y install docker-ce docker-ce-cli containerd.io
-sudo docker version
 sudo curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-docker-compose --version
 
 # Kubernets
 sudo apt-get -y install virtualbox
-VBoxManage -version
 curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
-kubectl version
 curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64 && chmod +x minikube
 sudo install minikube /usr/local/bin
 # rm minikube
-minikube version
 export CHANGE_MINIKUBE_NONE_USER=true
 sudo -E minikube start --vm-driver=none
 curl -Lo skaffold https://storage.googleapis.com/skaffold/releases/latest/skaffold-linux-amd64
 chmod +x skaffold
 sudo mv skaffold /usr/local/bin
+
+# Check Version
+git version
+node -v
+npm -v
+docker version
+docker-compose --version
+VBoxManage -version
+kubectl version
+minikube version
+skaffold version
